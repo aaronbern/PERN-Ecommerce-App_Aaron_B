5aed55f4c1ac7de84e73e3bd60a7214f9630aee5
Aaronbern@psu.edu <Aaronbern@psu.edu>
2023-08-18 14:13:05 -0700

    Normalize line endings

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const productRoutes = require('./routes/products');
-const cors = require('cors');
-app.use(cors());
-app.use('/products', productRoutes);
