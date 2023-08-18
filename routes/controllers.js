79d338aa7db66c082e255cab0425e8b5551dcfcb
Aaronbern@psu.edu <Aaronbern@psu.edu>
2023-08-18 13:34:27 -0700

    usercontroller started, modified package.json/server.js

--- routes/controllers.js
+++ routes/controllers.js
@@ -1,9 +1,38 @@
 const pool = require('../db');
 
-exports.getAllProducts = async (req, res) => {
+v
+exports.getProductById = async (req, res) => {
     try {
-        const results = await pool.query("SELECT * FROM products");
-        res.status(200).json(results.rows);
+        const { id } = req.params;
+        const result = await pool.query("SELECT * FROM products WHERE id = $1", [id]);
+
+        if (result.rows.length === 0) {
+            res.status(404).json({ message: "Product not found." });
+            return;
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.createProduct = async (req, res) => {
+    try {
+        const { name, price, expiration_date, quantity } = req.body;
+        
+        // Basic validation
+        if (!name || !price || !expiration_date || !quantity) {
+            res.status(400).json({ message: "Incomplete product data." });
+            return;
+        }
+
+        const result = await pool.query(
+            "INSERT INTO products (name, price, expiration_date, quantity) VALUES ($1, $2, $3, $4) RETURNING *",
+            [name, price, expiration_date, quantity]
+        );
+
+        res.status(201).json(result.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
 const productRoutes = require('./routes/products');
-
+const cors = require('cors');
+app.use(cors());
 app.use('/products', productRoutes);
