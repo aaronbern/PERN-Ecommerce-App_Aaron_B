9bae61ce874456bf8138799428ae7170363e971c
Aaronbern@psu.edu <Aaronbern@psu.edu>
2023-08-18 21:27:34 -0700

    Reinstalled node_modules, added swagger docu, setting up back end for testing

--- Back-end/routes/products.js
+++ Back-end/routes/products.js
@@ -2,10 +2,102 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     description: Retrieve a list of products
+ *     responses:
+ *       200:
+ *         description: List of products
+ */
 router.get('/', productsController.getAllProducts);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     description: Retrieve a product by its ID
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *         type: integer
+ *       required: true
+ *       description: ID of the product
+ *     responses:
+ *       200:
+ *         description: Single product
+ */
 router.get('/:id', productsController.getProductById);
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     description: Create a new product
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Created product
+ */
 router.post('/', productsController.createProduct);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     description: Update a product by its ID
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *         type: integer
+ *       required: true
+ *       description: ID of the product
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Updated product
+ */
 router.put('/:id', productsController.updateProduct);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     description: Delete a product by its ID
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *         type: integer
+ *       required: true
+ *       description: ID of the product
+ *     responses:
+ *       200:
+ *         description: Product deleted
+ */
 router.delete('/:id', productsController.deleteProduct);
 
 module.exports = router;
--- Back-end/server.js
+++ Back-end/server.js
@@ -1,15 +1,35 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const productRoutes = require('./routes/products');
 const userRoutes = require('./routes/users');
 
+const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.static('public')); // Optional: only if you're serving static files
-
+const swaggerOptions = {
+swaggerDefinition: {
+    info: {
+    version: "1.0.0",
+    title: "E-commerce API",
+    description: "E-commerce API Information",
+    contact: {
+        name: "Developer Name"
+    },
+    servers: ["http://localhost:YOUR_PORT_NUMBER"]
+    }
+},
+// ['./routes/*.js']
+apis: ["./routes/*.js"]
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Routes
 app.use('/products', productRoutes);
 
